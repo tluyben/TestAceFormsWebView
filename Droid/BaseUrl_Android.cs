4ef21a0e5579e0c66c62cac4412e830c03a1feb3
Tycho Luyben <tycho@e-lab.nl>
2015-02-25 17:54:44 +0100

    android version / untested

---
+++ Droid/BaseUrl_Android.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+using TestACEView.Droid; 
+
+[assembly: Dependency (typeof (BaseUrl_Android))]
+namespace TestACEView.Droid
+{
+	public class BaseUrl_Android : IBaseUrl {
+		public string Get () {
+			return "file:///android_asset/";
+		}
+	}
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TestAceFormsWebView
 ACE editor embedded in Xamarin Forms
+
+This is a basic mashup of ; 
+
+https://github.com/ajaxorg/ace-builds
+http://developer.xamarin.com/guides/cross-platform/xamarin-forms/working-with/webview/
